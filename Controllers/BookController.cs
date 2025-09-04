dd29b3d3c33bea2318c001260d786cbee35774e1
vannhi1604 <vannhi16042005@gmail.com>
2025-09-04 21:31:37 +0700

    thêm view cho Book/Index

---
+++ Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ltweb_bt2.Models;
+
+namespace ltweb_bt2.Controllers
+{
+    public class BookController : Controller
+    {
+        protected Book book = new Book();
+        public IActionResult Index()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            var books = book.GetBookList();
+            return View(books);
+        }
+    }
+}
